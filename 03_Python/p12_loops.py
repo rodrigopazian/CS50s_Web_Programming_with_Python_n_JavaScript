6a6103480bdbe1e1b463a4f975615134b47bca0f
rodrigopazian <rodrigopazian4322@gmail.com>
2022-08-08 19:03:58 -0300

    p12_loops.py criado

---
+++ 03_Python/p12_loops.py
@@ -0,0 +1,13 @@
+for i in [0, 1, 2, 3, 4, 5]:
+    print(i)
+
+for i in range(6):
+    print(i)
+
+namestr = "Harry"
+for letter in namestr:
+    print(letter)
+
+listnames = ["Harry", "Ron", "Hermione", "Ginny"]
+for name in listnames:
+    print(name)
