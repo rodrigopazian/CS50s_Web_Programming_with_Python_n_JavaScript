3d07cde1bd2d076a12c993a4492c4dfa661a17cc
rodrigopazian <rodrigopazian4322@gmail.com>
2022-08-08 19:55:24 -0300

    p13_dictionaries.py criado

---
+++ 03_Python/p13_dictionaries.py
@@ -0,0 +1,20 @@
+#Creating dictionary
+houses = {"Harry": "Gryffindor", "Draco": "Slytherin"}
+
+print(houses["Harry"])
+print(houses["Draco"])
+print(houses)
+
+
+#Adding Hermione
+houses["Hermione"] = "Gryffindor"
+
+print(houses["Hermione"])
+print(houses)
+
+
+#Changing Draco
+houses["Draco"] = "Sonserina"
+
+print(houses["Draco"])
+print(houses)
