834f0b4046a77e9dfe4983124e04987e8b0c7061
rodrigopazian <rodrigopazian4322@gmail.com>
2022-08-12 13:16:37 -0300

    Criado templates/hello/greet.html, função render com argumento n3 contexto utilizada

--- 04_Django/DjangoProject/hello/urls.py
+++ 04_Django/DjangoProject/hello/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("rodrigo", views.rodrigo, name="rodrigo"),
-    #path("<str:name>", views.greet, name="greet"),
+    path("<str:name>", views.greet, name="greet"),
     #path("<str:username>", views.user, name="user")
 ]
--- 04_Django/DjangoProject/hello/views.py
+++ 04_Django/DjangoProject/hello/views.py
@@ -3,14 +3,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    #return HttpResponse("Hello!")
+#    return HttpResponse("Hello!")
     return render(request, "hello/index.html")
 
 def rodrigo(request):
 	return HttpResponse("Hello, Rodrigo")
 
-# def greet(request, name):
-#     return HttpResponse(f"Hello, {name}!")
+def greet(request, name):
+#   return HttpResponse(f"Hello, {name}!")
+    return render(request, "hello/greet.html", {"name": name})
 
 #def user(request, username):
 #    return HttpResponse(f"Hello user, {username}!")
