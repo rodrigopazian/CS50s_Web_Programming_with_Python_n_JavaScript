0f60d7f1e14eb0195635cd232880cd67fe3f2981
rodrigopazian <rodrigopazian4322@gmail.com>
2022-08-12 10:51:17 -0300

    Adcionados url paths

--- 04_Django/DjangoProject/hello/urls.py
+++ 04_Django/DjangoProject/hello/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("rodrigo", views.rodrigo, name="rodrigo"),
+    #path("<str:name>", views.greet, name="greet"),
+    path("<str:username>", views.user, name="user")
 ]
--- 04_Django/DjangoProject/hello/views.py
+++ 04_Django/DjangoProject/hello/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return HttpResponse("Hello!")
+
+def rodrigo(request):
+	return HttpResponse("Hello, Rodrigo")
+
+def greet(request, name):
+    return HttpResponse(f"Hello, {name}!")
+
+def user(request, username):
+    return HttpResponse(f"Hello user, {username}!")
