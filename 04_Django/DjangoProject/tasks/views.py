8747b8db4a006f9c76eb44449a3a014af66f7b0a
rodrigopazian <rodrigopazian4322@gmail.com>
2022-08-15 11:53:26 -0300

    App tasks atualizado

--- 04_Django/DjangoProject/tasks/views.py
+++ 04_Django/DjangoProject/tasks/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
+#Global variables
+tasks = ["task1", "task2", "task3"]
+
 # Create your views here.
 def index(request):
-    return render(request, "tasks/index.html")
+    return render(request, "tasks/index.html", {"tasks": tasks})
     
