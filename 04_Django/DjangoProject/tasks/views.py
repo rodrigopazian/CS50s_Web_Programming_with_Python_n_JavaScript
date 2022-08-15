cefa3d78e35c78eeeee87b3ceca26f01fa46cb93
rodrigopazian <rodrigopazian4322@gmail.com>
2022-08-15 12:20:49 -0300

    addtask.html criado

--- 04_Django/DjangoProject/tasks/views.py
+++ 04_Django/DjangoProject/tasks/views.py
@@ -6,4 +6,7 @@ tasks = ["task1", "task2", "task3"]
 # Create your views here.
 def index(request):
     return render(request, "tasks/index.html", {"tasks": tasks})
+
+def addtask(request):
+    return render(request, "tasks/addtask.html")
     
