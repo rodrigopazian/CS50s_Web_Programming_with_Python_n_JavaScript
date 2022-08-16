1f765fa889deb17caecb1a2801ecf0eba59307eb
rodrigopazian <rodrigopazian4322@gmail.com>
2022-08-16 09:33:25 -0300

    Importado forms, e criado classe SubmmitNewTaskForm utilizando modulo django forms para criação de forms

--- 04_Django/DjangoProject/tasks/views.py
+++ 04_Django/DjangoProject/tasks/views.py
@@ -1,12 +1,18 @@
+from socket import fromshare
 from django.shortcuts import render
+from django import forms
 
 #Global variables
 tasks = ["task1", "task2", "task3"]
+
+class SubmmitNewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task: ")
+    priority = forms.IntegerField(label="Priority: ", max_value=10, min_value=1)
 
 # Create your views here.
 def index(request):
     return render(request, "tasks/index.html", {"tasks": tasks})
 
 def addtask(request):
-    return render(request, "tasks/addtask.html")
+    return render(request, "tasks/addtask.html", {"form": SubmmitNewTaskForm()})
     
