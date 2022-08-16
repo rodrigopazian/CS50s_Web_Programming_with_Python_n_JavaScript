77b0c0b3501d9564fc5d536b644ae82977a2c63b
rodrigopazian <rodrigopazian4322@gmail.com>
2022-08-16 10:11:28 -0300

    Adcionado tasks.append e metodo POST ao form

--- 04_Django/DjangoProject/tasks/views.py
+++ 04_Django/DjangoProject/tasks/views.py
@@ -6,13 +6,20 @@ from django import forms
 tasks = ["task1", "task2", "task3"]
 
 class SubmmitNewTaskForm(forms.Form):
-    task = forms.CharField(label="New Task: ")
-    priority = forms.IntegerField(label="Priority: ", max_value=10, min_value=1)
+    task = forms.CharField(label="New Task")
+    priority = forms.IntegerField(label="Priority", max_value=5, min_value=1)
 
 # Create your views here.
 def index(request):
     return render(request, "tasks/index.html", {"tasks": tasks})
 
 def addtask(request):
+    if request.method == "POST":
+        form = SubmmitNewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+        else:
+            return render(request, "tasks/addtask.html", {"form": form})
     return render(request, "tasks/addtask.html", {"form": SubmmitNewTaskForm()})
     
