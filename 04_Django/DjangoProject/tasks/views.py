81c4c4b778d2b4aba08b6da44b70e3177d3f56e8
rodrigopazian <rodrigopazian4322@gmail.com>
2022-08-16 12:20:09 -0300

    Adcionado lista de tarefas por sessão de usuário, aplicado comando migrate para criar lista de sessões

--- 04_Django/DjangoProject/tasks/views.py
+++ 04_Django/DjangoProject/tasks/views.py
@@ -4,7 +4,6 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 #Global variables
-tasks = []
 
 class SubmmitNewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
@@ -12,14 +11,18 @@ class SubmmitNewTaskForm(forms.Form):
 
 # Create your views here.
 def index(request):
-    return render(request, "tasks/index.html", {"tasks": tasks})
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
+    return render(request, "tasks/index.html", {"tasks": request.session["tasks"]})
 
 def addtask(request):
     if request.method == "POST":
         form = SubmmitNewTaskForm(request.POST)
         if form.is_valid():
             task = form.cleaned_data["task"]
-            tasks.append(task)
+            # tasks = request.session["tasks"]
+            # tasks.append(task)
+            request.session["tasks"] += [task]
             # return HttpResponseRedirect("/tasks")
             return HttpResponseRedirect(reverse("tasks:index"))
         else:
