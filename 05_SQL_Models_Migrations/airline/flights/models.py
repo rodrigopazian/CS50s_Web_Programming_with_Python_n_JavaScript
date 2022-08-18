db474f042df12fb07c07cb066d73dcb425122019
rodrigopazian <rodrigopazian4322@gmail.com>
2022-08-18 14:01:07 -0300

    Modelo Airport, criado, migrado, aplicadas migrações ao projeto

--- 05_SQL_Models_Migrations/airline/flights/models.py
+++ 05_SQL_Models_Migrations/airline/flights/models.py
@@ -1,9 +1,17 @@
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
+class Airport(models.Model):
+    city = models.CharField(max_length=64)
+    code = models.CharField(max_length=3)
+
+    def __str__ (self):
+        return f"Airport: {self.code} {self.city}"
+
 class Flight(models.Model):
-    origin = models.CharField(max_length=64)
-    destination = models.CharField(max_length=64)
+    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
     duration = models.IntegerField()
 
     def __str__ (self):
