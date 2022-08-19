aafee2d5a240d930d0d7f45bcd605ef202af20ee
rodrigopazian <rodrigopazian4322@gmail.com>
2022-08-19 13:45:20 -0300

    Criado flight.html

--- 05_SQL_Models_Migrations/airline/flights/views.py
+++ 05_SQL_Models_Migrations/airline/flights/views.py
@@ -5,3 +5,7 @@ from .models import Flight
 
 def index(request):
     return render(request, "flights/index.html", {"flights": Flight.objects.all()})
+
+def flight(request, flight_id):
+    flight = Flight.objects.get(id=flight_id)
+    return render(request, "flights/flight.html", {"flight": flight})
