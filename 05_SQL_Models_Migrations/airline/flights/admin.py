7f964f48faba59842857105e9cde90ad259efaa6
rodrigopazian <rodrigopazian4322@gmail.com>
2022-08-22 15:56:55 -0300

    Configuradas visualizações customizadas às vizualizações admin

--- 05_SQL_Models_Migrations/airline/flights/admin.py
+++ 05_SQL_Models_Migrations/airline/flights/admin.py
@@ -3,6 +3,12 @@ from django.contrib import admin
 from .models import Flight, Airport, Passenger
 
 # Register your models here.
+class FlightCustomView(admin.ModelAdmin):
+    list_display = ("id", "origin", "destination", "duration")
+
+class PassengerCustomView(admin.ModelAdmin):
+    filter_horizontal = ("passengerflights",)
+
 admin.site.register(Airport)
-admin.site.register(Flight)
-admin.site.register(Passenger)
+admin.site.register(Flight, FlightCustomView)
+admin.site.register(Passenger, PassengerCustomView)
