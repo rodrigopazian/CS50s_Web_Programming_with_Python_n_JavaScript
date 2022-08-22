1ad2572c3a23815de24a01d9393d32a4c4c2a2aa
rodrigopazian <rodrigopazian4322@gmail.com>
2022-08-22 11:49:06 -0300

    Adcionado view book

--- 05_SQL_Models_Migrations/airline/flights/views.py
+++ 05_SQL_Models_Migrations/airline/flights/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Flight
+from .models import Flight, Passenger
 # Create your views here.
 
 def index(request):
@@ -8,4 +10,12 @@ def index(request):
 
 def flight(request, flight_id):
     flight = Flight.objects.get(id=flight_id)
-    return render(request, "flights/flight.html", {"flight": flight, "passengers": flight.passengers.all()})
+    return render(request, "flights/flight.html", {"flight": flight, "passengers": flight.passengers.all(), "non_passengers": Passenger.objects.exclude(passengerflights=flight).all()})
+
+def book(request, flight_id):
+    if request.method == "POST":
+        flightid = Flight.objects.get(pk=flight_id)
+        passengerid = Passenger.objects.get(pk=int(request.POST["passengerid"]))
+        passengerid.passengerflights.add(flightid)
+        return HttpResponseRedirect(reverse("flights:flight", args=(flightid.id,)))
+        
